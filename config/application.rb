6f2142b5779d26677c2ae9459efb042712ed39c4
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2022-12-15 23:24:08 +0300

    fix hosts

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "rails-production-a459.up.railway.app"
   end
 end
