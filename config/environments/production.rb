82708809651b264d33b31dc36ee949c8d7f19ed9
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2022-12-15 23:18:50 +0300

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "rails-production-a459.up.railway.app"
+  config.hosts.clear
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_for :users
+  resources :parts
+  resources :products
+  resources :technologies
+  resources :vendors
+  resources :departments
+  resources :factories
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "factories#index"
 end
